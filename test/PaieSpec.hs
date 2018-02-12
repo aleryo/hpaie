a9fd5beb978562cd9121da25e38ce21281a077f1
Arnaud Bailly <arnaud.oqube@gmail.com>
2018-02-12 10:16:41 +0000

    Ajout charges maladies

--- test/PaieSpec.hs
+++ test/PaieSpec.hs
@@ -28,13 +28,31 @@ paie jourDePaie montantNet =
     ]
   ]
 
-spec :: Spec
-spec = describe "Generateur de paie" $ do
 
-  it "génere la paie"  $ do
+cotisationMaladie :: Date -> Montant -> Transaction
+cotisationMaladie date montant =
+  Tx "Maladie" date [ Debit  "645100:URSSAF" cotisation
+                    , Credit "431100:URSSAF" cotisation
+                    ]
+  where cotisation = 0.13 * montant
+
+
+spec :: Spec
+spec = describe "Générateur de paie" $ do
+
+  it "génère la paie"  $ do
     paie (date 2018 02 12) 1000 `shouldBe`
       [ Tx "Charges patronales" (date 2018 02 12)
         [ Debit  "100" 1000.00
         , Credit "200" 1000.00
         ]
       ]
+
+  describe "Charges patronales" $ do
+
+    it "calcul les charges maladies" $ do
+     cotisationMaladie (date 2018 02 12) 1000 `shouldBe`
+      Tx "Maladie" (date 2018 02 12)
+        [ Debit  "645100:URSSAF" 130.00
+        , Credit "431100:URSSAF" 130.00
+        ]
